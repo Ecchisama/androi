bbf709e11adb6bf43a86d869309162a0ae52fcfd
Ecchisama <92999664+Ecchisama@users.noreply.github.com>
2023-12-15 23:00:08 +0700

    Add files via upload

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,3 +4,41 @@ class Homepage extends StatefulWidget {
   @override
   _HomepageState createState() => _HomepageState();
 }
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 80,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40),
+              topRight: Radius.circular(40),
+            ),
+            color: Colors.grey[100],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              // IconButton(icon: Icon(Icons.hail), onPressed: () {}),
+              IconButton(icon: Icon(Icons.home), onPressed: () {}),
+              IconButton(icon: Icon(Icons.people), onPressed: () {}),
+              IconButton(icon: Icon(Icons.add_a_photo), onPressed: () {}),
+              IconButton(icon: Icon(Icons.account_balance), onPressed: () {}),
+              IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
